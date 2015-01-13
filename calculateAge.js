d8475629313ba1b1df803fd123adbfab5830aebd
ErikaLim <Erika.megan.lim@gmail.com>
2015-01-13 10:29:32 -0800

    calculateAge.js and maxFunction.js

---
+++ calculateAge.js
@@ -0,0 +1,5 @@
+function calculateAge (birth, current) {
+  var age = current - birth
+
+  return "you are either " + (age-1) + " or "+ age
+}
---
+++ maxFunction.js
@@ -0,0 +1,10 @@
+function maxNumber(number1, number2) {
+  if (number1 > number2) {
+    return number1
+  }
+  else if (number1 === number2) {
+    return number1 + "=" + number2
+  }
+  else
+    return number2
+}
